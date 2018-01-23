8f6306835461eb6f5a7782d98941da13b10a2630
fsacado <loann.meignant@hotmail.fr>
2018-01-23 19:57:19 +0100

    Identity administrable, show and edit done

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component
 /**
  * Project controller.
  *
- * @Route("project")
+ * @Route("admin/project")
  */
 class ProjectController extends Controller
 {
