87f9b7bef3fd24869404d5364fd83c6a557fe81e
fsacado <loann.meignant@hotmail.fr>
2018-01-30 09:30:26 +0100

    Fixing navbar scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,40 +2,35 @@ require('bootstrap-sass');
 require('jquery-scrollify');
 let $ = require('jquery');
 
-if ("ontouchstart" in document.documentElement) {
-    $.scrollify.disable();
-} else {
-    $(function () {
-        $.scrollify({
-            section: ".section"
-        });
-    });
-}
+$(document).ready(function () {
 
-// $(".nav a").on("click", function(e){
-//     $.scrollify("move", $(this).find("href").find("id"));
-// });
-// $('body').scrollspy({target: ".navbar"});
-// $(".nav a").on("click", function(){
-//    if (this.hash !== "") {
-//        event.preventDefault();
-//        let hash = this.hash;
-//        $('html, body').animate({
-//            scrollTop: $(hash).offset().top
-//        }, 800, function() {
-//            window.location.hash = hash;
-//        });
-//    }
-// });
-
-let navbarCollapse = function() {
-    if ($(".navbar").offset().top > 100) {
-        $(".navbar").addClass("navbar-shrink");
+    if ("ontouchstart" in document.documentElement) {
+        $.scrollify.disable();
     } else {
-        $(".navbar").removeClass("navbar-shrink");
+        $(function () {
+            $.scrollify({
+                section: ".section"
+            });
+        });
     }
-};
 
-// navbarCollapse();
-$(window).scroll(navbarCollapse);
+    let navbarCollapse = function () {
+        if ($(".navbar").offset().top > 100) {
+            $(".navbar").addClass("navbar-shrink");
+        } else {
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    };
 
+    navbarCollapse();
+    $(window).scroll(navbarCollapse);
+
+    $('.home').click(function () {
+        $.scrollify.move("#identity");
+    });
+
+    $('.projects').click(function () {
+        $.scrollify.move("#project1");
+    });
+
+});
